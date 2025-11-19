a874b6c20c3e94804ab0c8d3bc7f9c5583525eb5
hanzhuk <2443782944@qq.com>
2025-11-19 23:18:44 +0800

    7

--- searchdialog.cpp
+++ searchdialog.cpp
@@ -1,5 +1,6 @@
 #include "searchdialog.h"
 #include "ui_searchdialog.h"
+#include "QMessageBox"
 
 SearchDialog::SearchDialog(QWidget *parent,QPlainTextEdit * textEdit)
     : QDialog(parent)
@@ -8,6 +9,7 @@ SearchDialog::SearchDialog(QWidget *parent,QPlainTextEdit * textEdit)
     ui->setupUi(this);
 
     pTextEdit = textEdit;
+    ui->rbDown->setChecked(true);
 }
 
 SearchDialog::~SearchDialog()
@@ -23,12 +25,37 @@ void SearchDialog::on_btFindNext_clicked()
     QTextCursor c=pTextEdit->textCursor();
     int index = -1;
 
-    index = text.indexOf(target,c.position());
+    if(ui->rbDown->isChecked()){
+
+    index = text.indexOf(target,c.position(),ui->cbCaseSensetive->isChecked()?Qt::CaseSensitive : Qt::CaseInsensitive);
     if(index >=0){
         c.setPosition(index);
         c.setPosition(index +target.length(),QTextCursor::KeepAnchor);
 
         pTextEdit->setTextCursor(c);
+    }
+    }
+    else if(ui->rbUp->isChecked()){
+        index = text.lastIndexOf(target,c.position() - text.length() - 1,
+                                    ui->cbCaseSensetive->isChecked()?Qt::CaseSensitive : Qt::CaseInsensitive);
+        if(index >=0){
+            c.setPosition(index +target.length());
+            c.setPosition(index, QTextCursor::KeepAnchor);
+
+            pTextEdit->setTextCursor(c);
+        }
+
+    }
+
+
+    if(index < 0){
+        QMessageBox msg(this);
+        msg.setWindowTitle("记事本");
+        msg.setText(QString("找不到 ")+target);
+        msg.setWindowFlag(Qt::Drawer);
+        msg.setIcon(QMessageBox::Information);
+        msg.setStandardButtons(QMessageBox::Ok);
+        msg.exec();
     }
 
 
